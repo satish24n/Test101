5ce6ee974af57c0166e2a9e99a349d58f6a563b8
satish24n <android24n@gmail.com>
2018-03-31 02:10:20 +0530

    2nd commit with updated testng.xml and pom.xml

---
+++ src/test/java/org/start/Project102/One.java
@@ -0,0 +1,15 @@
+package org.start.Project102;
+
+import org.testng.annotations.Test;
+
+public class One {
+
+	@Test
+	public void one_tc1(){
+		System.out.println("one tc_01");
+	}
+	@Test
+	public void one_tc2(){
+		System.out.println("one tc_02");
+	}
+}
---
+++ src/test/java/org/start/Project102/Two.java
@@ -0,0 +1,16 @@
+package org.start.Project102;
+
+import org.testng.annotations.Test;
+
+public class Two {
+	
+	@Test
+	public void two_tc3(){
+		System.out.println("two tc_03");
+	}
+	@Test
+	public void two_tc4(){
+		System.out.println("two tc_04");
+	}
+	
+}
