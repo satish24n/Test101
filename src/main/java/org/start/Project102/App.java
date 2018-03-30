c5fde7057b326c8882ce9126dc14a49239aa8364
satish24n <android24n@gmail.com>
2018-03-31 01:32:20 +0530

    First commit project102

---
+++ src/main/java/org/start/Project102/App.java
@@ -0,0 +1,13 @@
+package org.start.Project102;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
